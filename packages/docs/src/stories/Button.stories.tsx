b5e1a67f292460376ce6c3c3f4145895207d7b59
Daniel Cazé <danielctlima@gmail.com>
2023-01-10 19:42:53 -0300

    feat: story button

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Button',
+  component: Button,
+  args: {
+    children: 'Button',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {
+  args: {},
+}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big',
+  },
+}
